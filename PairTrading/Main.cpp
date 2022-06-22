91beff2767181b7b7bd016a0211f95db6d939f95
Stephanie Becerra <stephanie.becerra.2018@gmail.com>
2022-06-21 17:51:12 -0700

    base csv reading and storing code
    this code is able to read and store the csv data in a vector vector allowing for rows and columns to exist to access later.

--- PairTrading/Main.cpp
+++ PairTrading/Main.cpp
@@ -4,11 +4,35 @@ Stephanie Becerra, Andrea Aguirre, Theresa Nguyen
 Pair Trading Project
 
 */
-#include <iostream>
 
-// Hello
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <regex>
+using namespace std;
+
+const regex comma(",");
 
 int main()
 {
-	return 0;
+    ifstream  data("SHEL.csv");
+    string line{};
+    vector<vector<string>> table;
+    while (data && getline(data, line)) {
+        // Tokenize the line and store result in vector. Use range constructor of std::vector
+        vector<string> row{ sregex_token_iterator(line.begin(),line.end(),comma,-1), sregex_token_iterator() };
+        table.push_back(row);
+    }
+
+    for (int i = 1; i < table.size(); i++) {
+        for (int j = 0; j < table[i].size(); j++) {
+            cout << table[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    //cout << table[3][1];
 }
